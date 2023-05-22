31255f7a12cae85d6b1e3643dac2b29aed973764
Rodrigo Oliveira <rodrigofeitosa.contato@gmail.com>
2023-05-22 19:52:05 -0300

    instalar tailwind e modificar layout
    instalação e configuração do tailwind e modificações no layout das paginas de cadastro-1 e login

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CPaaS-Telecall
+
+> Status: Em desenvolvimento
+
 Projeto CPaaS para a empresa Telecall em parceria com a faculdade Unisuam
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./pages/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
