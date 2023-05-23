7ff6d1891ab89da39a61b60e35bc9c23fcc256e3
Ingrid Gomes <ingridgomes81222@gmail.com>
2023-05-23 17:32:26 -0300

    criar cadastro-2 modificar header
    criação da pagina de cadastro-2 e mudança de estilo do header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["index.html", "./pages/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "Poppins" : ["Poppins", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        telecall :{
+          azul: "#0e4e7c",
+          vemelho: "#ca1c3a",
+          "vemelho-hover": "#99121f"
+        }
+      }
+    },
   },
   plugins: [],
 };
